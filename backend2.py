cdf82bb0873181a5ebdf3202f1c03f39b404d95e
pfc15 <pedrofcrux@gmail.com>
2024-04-09 12:41:28 -0300

    modificações do pedro/refatoreação pra linux64

--- backend2.py
+++ backend2.py
@@ -1,18 +1,20 @@
 import selenium
 from selenium import webdriver
+from selenium import By
 import requests
 from bs4 import BeautifulSoup
 #driver.get('https://sigaa.unb.br/sigaa/public/departamento/professores.jsf?id=673')
 link='https://sigaa.unb.br/sigaa/public/departamento/professores.jsf?id=673'
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0"}
 requisição=requests.get(link, headers=headers)
 site=BeautifulSoup(requisição.text, "html.parser")
-matéria= site.find_all("span", class_="pagina")
+matérias = site.find_all("span", class_="pagina")
 #link de cada professor e matéria
+driver = webdriver.Firefox()
 
-for c in matéria:
-    c=str(c)
-    link_editado=c.replace('<a href="','')
+for materia in matérias:
+    materia =str(materia)
+    link_editado= materia.replace('<a href="','')
     link_editado2=link_editado.replace('" target="_blank" title="Clique aqui para acessar a página pública deste docente">','')
     link_editado3=link_editado2.replace('Ver página','')
     link_editado4=link_editado3.replace('</a>','')
@@ -20,21 +22,25 @@ for c in matéria:
     link_editado6=link_editado5.replace('<span class="pagina">','')
     print(link_editado6)
     
-    driver = webdriver.Firefox()
+    
     driver.get(f'https://sigaa.unb.br{link_editado6}')
-    driver.find_element("xpath", '/html/body/dialog/button').click()
-    driver.find_element("xpath", '/html/body/div/div/div[2]/div[1]/ul/li[3]/a').click()
-    #esse é pra abrir a pagina disciplinas ministradas
-    driver.find_element("xpath", '/html/body/div/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div[1]/ul/li[6]/a[2]/em/span').click()
-    #clicar em graduação
+    try: #if (error)
+        driver.find_element("xpath", '/html/body/dialog/button').click() # 
+    except:
+        pass
+    driver.find_element("xpath", '/html/body/div/div/div[2]/div[1]/ul/li[3]/a').click() #esse é pra abrir a pagina disciplinas ministradas
+    driver.find_element("xpath", '/html/body/div/div/div[2]/div[2]/div[2]/div[1]/div/div[1]/div[1]/ul/li[6]/a[2]/em/span').click() #clicar em graduação
 
 
 
-    link2=(f'https://sigaa.unb.br{link_editado6}')
-    headers2 = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
-    requisição2=requests.get(link2, headers=headers)
-    site_bs4=BeautifulSoup(requisição.text, "html.parser")
-    codigo_materia=site.find_all("td", class_="codigo")
-    #disciplina_ministrada=site.find_all("a", name="href")
+
+    link2=(f'https://sigaa.unb.br{link_editado6[1:]}')
+    print(link2)
+    headers2 = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0"}
+    requisição2=requests.get(link2, headers=headers2)
+    site_bs4 = BeautifulSoup(requisição2.text, "html.parser")
+    codigo_materia= site_bs4.find_all("td", class_="codigo")
+    # disciplina_ministrada=site.find_all("a", name="href")
     print(f'{codigo_materia}','6')
+    break
     
