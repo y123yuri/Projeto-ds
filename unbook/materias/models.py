25ab8f84027e5a04f4d45a00cb48fda93d667cd5
pfc15 <pedrofcrux@gmail.com>
2024-04-22 13:47:07 -0300

    to string dos models

--- unbook/materias/models.py
+++ unbook/materias/models.py
@@ -6,11 +6,17 @@ class Professor(models.Model):
     nome = models.CharField(max_length=100, primary_key=True)
     foto = models.URLField()
 
+    def __str__(self):
+        return self.nome
+
 
 class Materia(models.Model):
     codigo = models.CharField(max_length=9, primary_key=True)
     nome = models.CharField(max_length=100)
     carga_horaria = models.CharField(max_length=10)
+    def __str__(self):
+        return self.codigo + " " + self.nome
+
 
 class Comentario(models.Model):
     autor_nick = models.CharField(max_length=100)
@@ -26,6 +32,8 @@ class Turma(models.Model):
     avaliação_1 = models.FloatField()
     avaliação_2 = models.FloatField()
     avaliação_3 = models.FloatField()
+    def __str__(self):
+        return self.materia.codigo + "/" + self.professor.nome
 
 
 
