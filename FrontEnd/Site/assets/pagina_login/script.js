1746cd9d554a8f2c2b51181291bce812edd41f06
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-04-25 18:14:21 -0300

    new

---
+++ FrontEnd/Site/assets/pagina_login/script.js
@@ -0,0 +1,19 @@
+function verificiar() {
+const verific = document.getElementById('box');
+const campos = document.querySelectorAll('.required');
+
+}
+
+function emailValidade () {
+    if(campos[0].) {
+
+        console.log('seu e-mail está incorreto');
+    }
+    else {
+        console.log('boa')
+    }
+}
+
+function setError(index) {
+    campos[index].style.border = '2px solid #e63636';
+}
