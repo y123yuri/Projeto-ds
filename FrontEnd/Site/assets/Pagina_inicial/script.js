ceeeb0b8b14703d126ab7910f38340f7ba3e6c92
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-04-25 14:18:43 -0300

    mudeidnv

---
+++ FrontEnd/Site/assets/Pagina_inicial/script.js
@@ -0,0 +1,14 @@
+function abrir_modal(){
+
+    var modal = document.getElementById('janela_pesquisa');
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click',fechar_modal);
+}
+
+function fechar_modal () {
+    var modal= document.getElementById('janela_pesquisa');
+
+    
+
+}
