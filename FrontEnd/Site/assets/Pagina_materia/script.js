0e0c59b2d2807c86273bd5bb817e543877b82180
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-04-30 01:52:46 -0300

    new

---
+++ FrontEnd/Site/assets/Pagina_materia/script.js
@@ -0,0 +1,6 @@
+function reproduzirMateria2(materias) {
+    console.log('vizinho tb')
+    var reproduzir = materias.textContent
+
+    document.getElementById('CampoFalado2').textContent = reproduzir;
+}
