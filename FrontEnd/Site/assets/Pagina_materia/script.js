a7db0b7250853a0d8381bfb1a32162d536137649
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-04-30 02:18:05 -0300

    new

--- FrontEnd/Site/assets/Pagina_materia/script.js
+++ FrontEnd/Site/assets/Pagina_materia/script.js
@@ -4,3 +4,19 @@ function reproduzirMateria2(materias) {
 
     document.getElementById('CampoFalado2').textContent = reproduzir;
 }
+
+
+var stars = document.querySelectorAll('.star-icon');
+
+document.addEventListener('click', function (e) {
+    var classStar = e.target.classList;
+    
+    if (!classStar.contains('ativo')) {
+        stars.forEach(function (star) {
+            star.classList.remove('ativo');
+        });
+        classStar.add('ativo');
+        console.log(e.target.getAttribute('data-avaliacao'));
+    }
+});
+
