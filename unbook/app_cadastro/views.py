f11a25f79b080e75cdf26eda23d39eb85642f9ba
Pedro Cruz <pedrofcrux@gmail.com>
2024-05-06 12:19:50 -0300

    lida com erros no cadastro

--- unbook/app_cadastro/views.py
+++ unbook/app_cadastro/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CadastroForm
 from django.http import HttpResponse
 from .models import Cadastro
@@ -9,18 +9,21 @@ def cadastro(request):
     context = {}
     form = CadastroForm()
     context["form"] = form
+    if request.method == "POST":
+        context['erro'] = "já existe um cadastro com o email ou nome de usuario"
     return render(request, "html/cadastro.html", context)
 
 def sucesso(request):
     obj = Cadastro()
     f = CadastroForm(request.POST, instance=obj)
     context = {}
+    if 'erro' in request.session:
+        del request.session['erro']
     if f.is_valid():
-        
-        print(type(f), type(f.cleaned_data), type(obj))
+        print(type(f), f.cleaned_data, type(obj))
         context["resposta"] = f.cleaned_data
         obj.save()
     else:
-        context["resposta"] = False
-    
+        request.session['erro'] = "já existe um cadastro com o email ou nome de usuario"
+        return redirect("../")
     return render(request, "html/cadastro_sucesso.html", context)
--- unbook/static/js/cadastro_sucesso.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",function() {
-
-    var reproduzirNome = localStorage.getItem("StorageNome");
-
-    document.getElementById("NomeRecebido").innerText = reproduzirNome;
-
-});
