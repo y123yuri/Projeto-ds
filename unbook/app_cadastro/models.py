1d5904a6f278f0ea56161d57f4be90d81f475ceb
Pedro Cruz <pedrofcrux@gmail.com>
2024-05-06 11:56:05 -0300

    conserto do merge

--- unbook/app_cadastro/models.py
+++ unbook/app_cadastro/models.py
@@ -3,13 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Cadastro(models.Model):
-<<<<<<< HEAD
     nome_usuario = models.CharField(max_length=150, null=False, blank=False, unique=True)
     email = models.EmailField(max_length=22, null=False, blank=False, unique=True, primary_key=True)
-=======
-    nome_usuario = models.CharField(max_length=150, null=False, blank=False)
-    email = models.EmailField(max_length=50, null=False, blank=False)
->>>>>>> 098859d47ef34f5d02a0d1fbb6c0a66ccd05f8c4
     senha = models.CharField(max_length=30, null=False, blank=False)
 
     def __str__(self):
--- unbook/app_cadastro/urls.py
+++ unbook/app_cadastro/urls.py
@@ -4,10 +4,6 @@ from app_cadastro import views as v
 
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.cadastro, name="cadastro"),
-=======
-    # path('', views.cadastro, name="cadastro"),
->>>>>>> 098859d47ef34f5d02a0d1fbb6c0a66ccd05f8c4
     path('sucesso/', views.sucesso, name="sucesso"),
 ]
