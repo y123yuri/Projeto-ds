e4534ccbdd7ea59867b2b26a23bffb046dd4d255
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-05-07 16:26:28 -0300

    new

---
+++ FrontEnd/Site/assets/js/drop_files_(Livros.html).js
@@ -0,0 +1,22 @@
+const dropArea = document.getElementById('label_file');
+const input = document.getElementById('input_file');
+const imgView = document.getElementById('imgView');
+
+input.addEventListener('change', Upload_image)
+
+function Upload_image() {
+    input.files[0];
+    let imgLink = URL.createObjectURL(input.files[0]);
+    imgView.style.backgroundImage = `url(${imgLink})`;
+    imgView.textContent = '';
+}
+
+dropArea.addEventListener("dragover", function(e) {
+    e.preventDefault();
+})
+
+dropArea.addEventListener("drop", function(e) {
+    e.preventDefault();
+    input.files = e.dataTransfer.files;
+    Upload_image();
+})
