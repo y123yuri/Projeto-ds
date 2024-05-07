34288212bcc2cfa80eac4c0e8b82bb41d74edcb9
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-05-07 12:12:47 -0300

    new

--- FrontEnd/Site/assets/js/abrir_modais_(Livros.html).js
+++ FrontEnd/Site/assets/js/abrir_modais_(Livros.html).js
@@ -1,7 +1,7 @@
 var fundo_blur = document.getElementById('fundo_blur');
 var modal = document.getElementById('modal_upload');
 
-function abrir_modal () {
+function abrir_modal() {
 
     fundo_blur.style.display = 'block';
 
@@ -14,44 +14,44 @@ function enviar() {
     var link_enviado = document.getElementById('poe_links').value;
     var nome_link = document.getElementById('queLinkéEsse').value;
 
-if (link_enviado !== '' && nome_link !== '') {
-    var scroll = document.getElementById('scroll')
-    
-    var nome = document.createElement('h2')
-    nome.className = 'nomeURL'
-    nome.innerText = (nome_link + ': ');
-    
-    var link = document.createElement('a')
-    link.innerText = link_enviado;
-    
-    link.className = 'links';
-    link.href =  link_enviado;
-    link.target = '_blank';
+    if (link_enviado !== '' && nome_link !== '') {
+        var scroll = document.getElementById('scroll')
 
-    var pula_linha = document.createElement('hr')
-    
-    scroll.appendChild(nome);
-    scroll.appendChild(link);
-    scroll.appendChild(pula_linha);
-    
-    link_enviado.value = '';
-    nome.value = '';
-    
-    fechar_modal();
-} else {
-    alert('Tem que ter um link ai dentro man')
-}
-    
+        var nome = document.createElement('h2')
+        nome.className = 'nomeURL'
+        nome.innerText = (nome_link + ': ');
+
+        var link = document.createElement('a')
+        link.innerText = link_enviado;
+
+        link.className = 'links';
+        link.href = link_enviado;
+        link.target = '_blank';
+
+        var pula_linha = document.createElement('hr')
+
+        scroll.appendChild(nome);
+        scroll.appendChild(link);
+        scroll.appendChild(pula_linha);
+
+        link_enviado.value = '';
+        nome.value = '';
+
+        fechar_modal();
+    } else {
+        alert('Tem que ter um link ai dentro man')
+    }
+
 }
 
-document.addEventListener('click', function(e) {
-    if(e.target === fundo_blur) {
+document.addEventListener('click', function (e) {
+    if (e.target === fundo_blur) {
         fundo_blur.style.display = 'none';
         modal.style.display = 'none';
-    } 
+    }
 })
 
 function fechar_modal() {
     fundo_blur.style.display = 'none';
-        modal.style.display = 'none';
+    modal.style.display = 'none';
 }
