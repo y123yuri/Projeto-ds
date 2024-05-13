521894b1882a65bf20d6607a3fc3bf015a6f4024
Szervinsk <165739040+Szervinsk@users.noreply.github.com>
2024-05-13 15:11:01 -0300

    ?

--- unbook/app_cadastro/forms.py
+++ unbook/app_cadastro/forms.py
@@ -7,8 +7,11 @@ class CadastroForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CadastroForm, self).__init__(*args, **kwargs)
         self.fields['nome_usuario'].widget.attrs = {'class':"campoEscrito , required", "id":"CampoNome", "name":"Nome", "type":"text", "placeholder":"Usuario"}
+
         self.fields['email'].widget.attrs = {"class":"campoEscrito , required", "name":"Email", "type":"email", "placeholder":"Matrícula@aluno.unb.com"}
+
         self.fields['senha'].widget = forms.PasswordInput()
+        
         self.fields['senha'].widget.attrs = {"class":"campoEscrito , required",  "name":"Senha", "type":"password", "placeholder":"*******"}
 
     class Meta:
