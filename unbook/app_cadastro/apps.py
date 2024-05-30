e1e3799a73a7a2c2831926e9b66e59954d3f7c1c
y123yuri <dayannadir.2011@gmail.com>
2024-05-30 15:44:26 -0300

    commit das coisas funcionando

--- unbook/app_cadastro/apps.py
+++ unbook/app_cadastro/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 class AppCadastroConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_cadastro'
-    # def ready(self):
-    #     import app_cadastro.signals
+    def ready(self):
+        import app_cadastro.signals
