3c9ffa1343c073485f4a15f2c02d8b8084ad2178
y123yuri <dayannadir.2011@gmail.com>
2024-07-04 12:19:26 -0300

    tirandp dados importantes

--- unbook/unbook/settings.py
+++ unbook/unbook/settings.py
@@ -131,10 +131,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'unbook.br@gmail.com'
-
-DEFAULT_FROM_EMAIL = 'unbook.br@gmail.com'
-
-EMAIL_HOST_PASSWORD = 'olossrchikhyvjux'
