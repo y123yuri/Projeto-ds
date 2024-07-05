e3e92880932429df1643576712bf63ccabab8e62
pedrofcruz <pedrofcrux@gmail.com>
2024-07-05 09:58:40 -0300

    novas analises

--- analise_dados/get_data.py
+++ analise_dados/get_data.py
@@ -6,19 +6,14 @@ cnx = sqlite3.connect('../unbook/db.sqlite3')
 
 # Read table into DataFrame
 users_df = pd.read_sql_query("SELECT * FROM app_cadastro_perfilusuario;", cnx)
+professor_df = pd.read_sql_query("SELECT * FROM materias_professor;", cnx)
+materias_df = pd.read_sql_query("SELECT * FROM materias_materia;", cnx)
 turmas_df = pd.read_sql_query("SELECT * FROM materias_turma;", cnx)
+comentario_df = pd.read_sql_query("SELECT * FROM materias_comentario;", cnx)
 
-print(len(users_df))
-print(turmas_df.head(10))
-print(turmas_df.columns)
-print(len(turmas_df))
+print(f'Coletamos  {len(professor_df)} professores e {len(materias_df)} com {len(turmas_df)} no web scrapping')
+print('--'*25)
+print(f'Tivemos {len(users_df)} cadastros feitos, {len(comentario_df)} comentários feitos')
 
-codigos = turmas_df["materia_id"].apply(lambda x: x[:3])
-print("-="*15)
-print(codigos)
 
-fig = px.histogram(codigos, x='materia_id', title='Histogram of Parsed Strings', log_y=True)
-
-# Show the plot
-fig.show()
 
