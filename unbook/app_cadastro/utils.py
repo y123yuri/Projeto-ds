158979a7c080520d9d1330973aca349e31757058
y123yuri <dayannadir.2011@gmail.com>
2024-07-13 13:12:42 -0300

    arrumado esqueceu a senha

--- unbook/app_cadastro/utils.py
+++ unbook/app_cadastro/utils.py
@@ -12,6 +12,35 @@ from django.contrib.auth.tokens import default_token_generator
 
 from django.urls import reverse
 
+from django.utils.http import urlsafe_base64_encode
+
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+
+
+def send_password_reset_email(user, request):
+    token = default_token_generator.make_token(user)
+    
+    try:
+        reset_url = request.build_absolute_uri(
+            reverse('novaSenha', kwargs={'token': token})
+        )
+    except Exception as e: 
+ 
+        raise e       
+
+    subject = "Redefinição de senha"
+    message = render_to_string('html/redefinição.html', {
+        'user': user,
+        'reset_url': reset_url
+    })
+
+    try:
+        send_mail(subject, message, 'unbook.br@gmail.com', [user.email])
+      
+    except Exception as e:
+
+        raise e
 
 def send_activation_email(user, request):
 
