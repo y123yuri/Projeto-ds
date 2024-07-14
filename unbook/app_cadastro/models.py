c061f52eb45f968c8a4b73993f30ef9e4b121b47
SchneiderCode1 <165057013+SchneiderCode1@users.noreply.github.com>
2024-07-14 16:16:12 -0300

    Esqueci a senha consertado (vamo porra)

--- unbook/app_cadastro/models.py
+++ unbook/app_cadastro/models.py
@@ -35,10 +35,13 @@ class Cadastro(models.Model):
 
 # models.py
 
+def generate_uuid():
+    return str(uuid.uuid4())
 
 class PasswordResetToken(models.Model):
+    id = models.UUIDField(primary_key=True, default=generate_uuid, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    token = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def is_valid(self):
